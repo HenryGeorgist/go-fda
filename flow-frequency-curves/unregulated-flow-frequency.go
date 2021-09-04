064101e3794fb2f046e36474073d73751814c7ce
HenryGeorgist <william.p.lehman@usace.army.mil>
2021-09-04 17:34:20 +0000

    basic curves for each relationship

---
+++ flow-frequency-curves/unregulated-flow-frequency.go
@@ -0,0 +1,23 @@
+package flowfrequencycurves
+
+import (
+	"github.com/HydrologicEngineeringCenter/go-statistics/statistics"
+	"github.com/USACE/go-consequences/paireddata"
+)
+
+type UnregulatedFlowFrequehcyCurve struct {
+	Distribution            statistics.ContinuousDistribution
+	EquivalantYearsOfRecord int
+	Ordinates               int
+}
+
+func (uffc UnregulatedFlowFrequehcyCurve) Sample(randomValue float64) paireddata.ValueSampler {
+	flows := make([]float64, uffc.Ordinates)
+	probs := make([]float64, uffc.Ordinates)
+	//TODO: use random value to seed a bootstrap on the distribution to create a bootstrap
+	for i := 0; i < uffc.Ordinates; i++ {
+		probs[i] = float64(float64(i)+0.5) / float64(uffc.Ordinates)
+		flows[i] = uffc.Distribution.InvCDF(probs[i])
+	}
+	return paireddata.PairedData{Xvals: probs, Yvals: flows}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/HenryGeorgist/go-fda
 
 go 1.15
+
+require (
+	github.com/HydrologicEngineeringCenter/go-statistics v0.0.0-20210904151206-04811e288d78
+	github.com/USACE/go-consequences v0.0.0-20210814143036-e883a988b5af
+)
