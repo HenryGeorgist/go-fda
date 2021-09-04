79d52baa93dfc1a6d9a9575b904648449a44722e
HenryGeorgist <william.p.lehman@usace.army.mil>
2021-09-04 18:15:04 +0000

    work in progress ead compute

--- compute/ead.go
+++ compute/ead.go
@@ -1,9 +1,12 @@
 package compute
 
 import (
+	"fmt"
+
 	"github.com/HenryGeorgist/go-fda/flowfrequencycurves"
 	"github.com/HenryGeorgist/go-fda/ratingcurves"
 	"github.com/HenryGeorgist/go-fda/stagedamagecurves"
+	"github.com/USACE/go-consequences/paireddata"
 )
 
 type ExpectedAnnualDamage struct {
@@ -13,10 +16,29 @@ type ExpectedAnnualDamage struct {
 }
 
 func (ead ExpectedAnnualDamage) Compute() float64 {
-	/*
-		ff := ead.FlowFrequency.Sample(.5)
-		rc := ead.RatingCurve.Sample(.5)
-		dc := ead.DamageCurve.Sample(.5)
-	*/
+	ff := ead.FlowFrequency.Sample(.5)
+	ffpd, ffok := ff.(paireddata.PairedData)
+	if !ffok {
+		panic("frequency curve is not paired data.")
+	}
+	rc := ead.RatingCurve.Sample(.5)
+	rcpd, rcok := rc.(paireddata.PairedData)
+	if !rcok {
+		panic("rating curve is not paired data.")
+	}
+	dc := ead.DamageCurve.Sample(.5)
+	dcpd, dcok := dc.(paireddata.PairedData)
+	if !dcok {
+		panic("stage damage curve is not paired data.")
+	}
+	fmt.Println(dcpd)
+	//compose flow frequeny with flow stage
+	for idx, flow := range rcpd.YVals {
+		prob := ffpd.SampleValue(flow)
+		stage := rcpd.XVals[idx]
+		fmt.Println(prob)
+		fmt.Println(stage)
+	}
+
 	return .5
 }
